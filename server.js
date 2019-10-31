d524a885cd000bbd31cdd30898c27e5fccbae830
Paul Palma <palmachato@yahoo.com>
2019-10-30 21:43:27 -0400

    adding code to server.js

--- server.js
+++ server.js
@@ -29,8 +29,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/news-hw", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/news-hw", { useNewUrlParser: true });
 
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-hw";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 // Routes
 app.get('/', function (req, res) {
     res.sendFile('index.html')
